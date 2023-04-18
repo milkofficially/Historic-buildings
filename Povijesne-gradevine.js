0e3551d6e6896bc4fbfbaac3e40dc79db8ec221d
milkofficially <125404941+milkofficially@users.noreply.github.com>
2023-04-19 00:41:45 +0200

    Add files via upload

---
+++ Povijesne-gradevine.js
@@ -0,0 +1,21 @@
+function kuršumlija() {
+    document.getElementById('naslov').innerHTML = "Jusuf-pašina 'Kuršumlija'";
+    document.getElementById('tekst').innerHTML = "Džamiju je izgradio Ali-paša Kalauz, koji je na carskom dvoru imao titulu beglerbega. U Budimu je pašovao 1578. a u Bosni 1584. Kasnije je imenovan vezirom i pozvan u Carigrad, oženio udovicu Sokolovića Ismihan sultaniju. Kad je ova umrla, po drugi put je imenovan u Budimu, gdje je umro 1586. Njegova džamija poznata je u narodu pod imenom Kuršumlija, jer je pokrivena olovom. Nalazi se u središtu starog Maglaja, koji je do danas sačuvao svoje istočnjačko obilježje. Kako piše Ćiro Truhelka:    ... s njom bi se mogao ponositi svaki veliki grad" + "<br>" + "<h1>Arhitektura</h1>" + "Džamija je četvrtastog oblika, pokrivena visokim kubetom, a pred njom je prostran trijem s tankim kamenim stupovima, koji nose svodovlje arkada i manjih kupola, što pokrivaju trijem. Sazidana je od vrlo pomno otesanih kamenica. Zanimljivija od same džamije je munara s prekrasnim vijencem stalaktita, koji nosešerefe, s koje mujezin zove muslimane na namaz. Tu munaru pokrio je šiljati olovni krov. " + "<h1>Prve restauracije</h1>" + "Popravak Kuršumlije u 1867. godini bi mogao biti drugi popravak, jer je vrlo vjerovatno jedan, uslijedio nakon demoliranja džamije od strane Savojskog 1697. godine. Prema do danas sačuvanom predanju, nad ulazom u Kuršumliju nekada stajaše zlatna ploča sa natpisom (svakako sa pozlaćenim slovima), koju su Eugenovi vojnici odnijeli sa sobom kao ratni trofej. Iz tariha iznad ulaznih vrata, iznutra, stoji da je bio «tamir» - popravak džamije i u 1318. po H., odnosno u godini 1900-02. i to bi bio treći po redu. Četvrti je počeo 1972. godine. Džamija Kuršumlija je bila 1900. godine farbana. Godine 1902. vjetar je iznad šerefa prelomio munaru, koja je nanovo sazidana i postavljen alem. ";
+    document.getElementById("slika-1").src = "kuršumlija-1.jpg";
+    document.getElementById("slika-2").src = "Image03.jpg";
+    document.getElementById("slika-3").src = "kuršumlija-3.jpg";
+    document.getElementById("slika-1").style.visibility = "visible";
+    document.getElementById("slika-2").style.visibility = "visible";
+    document.getElementById("slika-3").style.visibility = "visible";
+
+}
+function delibegovhan() {
+    document.getElementById('naslov').innerHTML = "Delibegov-han";
+    document.getElementById('tekst').innerHTML = "Hanovi i karavan-saraji nicali su na svim putnim pravcima pa tako je Maglaj dobio svoj, Delibegov han na lijevoj obali Bosne.Podignut je u drugoj polovini XIX vijeka i dugo je bio jedini objekat u maglajskom polju.Delibegov han je u zadnjem ratu bio potpuno devastiran i gotovo izgubljen. Tokom poslijeratnog razdoblja - poput mnogih vrijednih objekata kulturne i graditeljske baštine, tako je i ovaj spomenik graditeljskog naslijeđa pretrpio dodatnu štetu zbog izloženosti vremenskim i ostalim nepovoljnim utjecajima vlaga, promjena temperature, itd..Han je obnovljen 2013. godine u sklopu programa “Pripremne akcije za očuvanje i obnovu kulturne baštine u područjima sukoba na Zapadnom Balkanu za 2010“ koji je finansirala Europska unija, a implementiran je u partnerstvu Regionalne razvojne agencije za regiju Centralna BiH i Općine Maglaj.";
+    document.getElementById("slika-1").src = "deliegov-han-1.jpg";
+    document.getElementById("slika-2").src = "deliegov-han-2.jpg";
+    document.getElementById("slika-3").src = "deliegov-han-3.jpg";
+    document.getElementById("slika-1").style.visibility = "visible";
+    document.getElementById("slika-2").style.visibility = "visible";
+    document.getElementById("slika-3").style.visibility = "visible";
+}
